3e214c872020c5312d46ba2c65ae5e6935269cd6
Nguyễn Hoàng Lộc <hoanglocst900@gmail.com>
2022-12-21 16:37:37 +0700

    Setup swagger

---
+++ src/core/router.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+
+export const router = (app: INestApplication) => {
+  app.setGlobalPrefix('api/v1');
+
+  // Setup Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Base NestJS API')
+    .setDescription('The base NestJS API description')
+    .setVersion('0.1.0')
+    .addTag('base')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/explorer', app, document);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config/dist';
+import { router } from './core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
+  router(app);
   await app.listen(configService.get<number>('port'));
 }
 bootstrap();
