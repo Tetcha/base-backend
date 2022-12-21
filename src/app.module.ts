2f30e4ec1fce170294bd6a1435be03f14bfdea80
loc <hoanglocst900@gmail.com>
2022-12-21 21:43:49 +0700

    Init typeorm & db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { configuration } from './config';
 
 @Module({
@@ -8,6 +9,20 @@ import { configuration } from './config';
       envFilePath: '.env.development',
       load: [configuration],
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: +configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,5 +3,8 @@ export const configuration = () => ({
   database: {
     host: process.env.DATABASE_HOST || 'localhost',
     port: Number(process.env.DATABASE_PORT) || 5432,
+    username: process.env.DATABASE_USERNAME || 'root',
+    password: process.env.DATABASE_PASSWORD || '1234',
+    name: process.env.DATABASE_NAME || 'base_nest',
   },
 });
